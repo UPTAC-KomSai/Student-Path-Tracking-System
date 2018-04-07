b7523739539e0836a6dabf25c66171ecfb7a7791
cycole1234 <cycole1234@gmail.com>
2018-04-08 02:56:36 +0800

    Dashboard update

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,18 +1,37 @@
 class RecordsController < ApplicationController
+  @stud_info
   def index 
   end
 
   def home
-    a = Mechanize.new
-    stud_id = params[:student_number]
-    password = params[:password]  	
-    String url = 'http://crs.upv.edu.ph/tacloban/student/loginAuthenticate.jsp?studentIDYear=&studentIDNumber='+stud_id+'&password='+password
+      a = Mechanize.new
+      stud_id = params[:student_number]
+      password = params[:password]  	
+      
+      e = "Student ID|:|Name|Degree Level|Degree Program|Year Level|Scholarship"
+      
+      if(password != nil and stud_id != nil)
+        String url = 'http://crs.upv.edu.ph/tacloban/student/loginAuthenticate.jsp?studentIDYear=&studentIDNumber='+stud_id+'&password='+password
         
-    login = a.get(url)
-    
-    if(login.uri.to_s.include?("errorMsg"))
-      redirect_to action: "index"
-    end
+        login = a.get(url)
+        @stud_info = (login.search(".//td[@class='labelleftbrown8B']").text).to_s
+        @stud_info =  @stud_info.gsub(/\s+/, " ")
+   
+        @stud_info.gsub!(/#{e}/, '@')
+        d = @stud_info.split("@")
+        info = Array.new
+        d.each do |w|
+            if !(w.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '') == "")
+              info << w.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
+            end
+        end
+        
+        @stud_info = info
+        
+        if(login.uri.to_s.include?("errorMsg"))
+          redirect_to action: "index"
+        end
+      end
   end
   
   def personalProfile
